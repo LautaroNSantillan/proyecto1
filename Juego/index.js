cd9c02e557882bfe0a331e8d4c3d69de38f8eb5f
SantillanLautaro <santillanlautaronahuel@gmail.com>
2022-09-17 23:50:08 -0300

    ObjetosVelocidadPosicion

--- Juego/index.js
+++ Juego/index.js
@@ -6,29 +6,49 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-class Sprite{
-    constructor(position){
+class Sprite {
+    constructor({ position, velocity }) {
         this.position = position
+        this.velocity = velocity
     }
 
-    draw(){
+    draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, 50, 150)
     }
 }
 
 const player = new Sprite({
-    x: 0,
-    y: 0   
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 })
 
 player.draw()
 
 const enemy = new Sprite({
-    x: 400,
-    y: 100   
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 })
 
 enemy.draw()
 
 console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    console.log('go')
+}
+
+animate()
